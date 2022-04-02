0cf7832eb0a428ebadb3402960efb94d1ddc9438
Kunmun <kunmun.devroms@gmail.com>
2022-04-02 09:13:50 +0530

    X00T: liblight: Import from LA.UM.10.2.1.r1-03400-sdm660.0
    • Move to Android.bp as well
    
    Change-Id: Ia75a4aa96b559fcb2ba006bab697f450265060fb
    Signed-off-by: karthik1896 <karthik1896@outlook.com>

--- device.mk
+++ device.mk
@@ -98,6 +98,11 @@ PRODUCT_COPY_FILES += \
 # Kernel
 TARGET_KERNEL_VERSION := 4.4
 
+# Lights
+PRODUCT_PACKAGES += \
+    lights.qcom \
+    android.hardware.lights-service.qti
+
 # Media
 PRODUCT_PACKAGES += \
     android.hardware.media.omx@1.0-impl \
