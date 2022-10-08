travarilo/device_asus_X00T

X00T: create libGLESv2_adreno.so symlink
* libGLESv2_adreno.so is needed on camera blobs

Change-Id: Iced972a02073e0c327e02a359870aeec299af0bd
Signed-off-by: Genkzsz11 <genkzsz11@gmail.com>

Android.mk
@@ -139,4 +139,11 @@ $(SNAP_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 
 ALL_DEFAULT_INSTALLED_MODULES += $(SNAP_SYMLINKS)
 
+EGL_SYMLINK := $(TARGET_OUT_VENDOR)/lib/libEGL_adreno.so
+$(EGL_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@mkdir -p $(dir $@)
+	$(hide) ln -sf egl/$(notdir $@) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(EGL_SYMLINK)
+
 endif

Project404-android-asus-sdm660/android_device_asus_X00T

Signed-off-by: karthik1896 <karthik1896@outlook.com>

Android.mk
@@ -90,4 +90,11 @@ $(WCNSS_INI_SYMLINK): $(LOCAL_INSTALLED_MODULE)
 
 ALL_DEFAULT_INSTALLED_MODULES += $(WCNSS_INI_SYMLINK)
 
+EGL_SYMLINK := $(TARGET_OUT_VENDOR)/lib/libGLESv2_adreno.so
+$(EGL_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@mkdir -p $(dir $@)
+	$(hide) ln -sf egl/$(notdir $@) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(EGL_SYMLINK)
+
 endif